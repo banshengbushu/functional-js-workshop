283d4da29fe0676ff8408a76b5a7bdc1ceba904f
Danna Wang <danna.wang@compass.com>
2020-09-21 19:29:42 +0800

    Improve recursion without for loop

--- recursion.js
+++ recursion.js
@@ -1,23 +1,16 @@
 function reduce(arr, fn, initial) {
-  if (arr.length === 0) return;
-
-  let result = initial;
-  const next = arr.slice(1);
-
-  result = Object.assign(
-    {},
-    fn(initial, arr[0], 0, arr),
-    reduce(next, fn, result)
-  );
-
-  return result;
+  const reduceOne = (index, result) => {
+    if (index > arr.length - 1) return result;
+    return reduceOne(index + 1, fn(result, arr[index], index, arr));
+  };
+  return reduceOne(0, initial);
 }
 
 module.exports = reduce;
 
 // function reduce(arr, fn, initial) {
 //   return (function reduceOne(index, value) {
-//     if (index > arr.length - 1) return value // end condition
-//     return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step
-//   })(0, initial) // IIFE. kick off recursion with initial values
+//     if (index > arr.length - 1) return value; // end condition
+//     return reduceOne(index + 1, fn(value, arr[index], index, arr)); // calculate & pass values to next step
+//   })(0, initial); // IIFE. kick off recursion with initial values
 // }
